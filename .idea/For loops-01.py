f01facc1132e411283e19cb38b31600eccd7f17f
Mutasir005 <muntasirulislam2005@gmail.com>
2024-10-07 19:43:15 +0600

    for loop related problems

---
+++ .idea/For loops-01.py
@@ -0,0 +1,9 @@
+for items in 'Python':
+    print(items)
+for numbers in range (5,10,2):
+    print(numbers)
+for names in ['MOSH','JOSH']:
+    print(names)
+for x in range(4):
+    for y in range(5):
+        print(f'{x},{y}')
---
+++ .idea/inspectionProfiles/For loop-03.py
@@ -0,0 +1,6 @@
+print("Question: Write a code to calculate the total of the prices in the cart")
+prices=[20,30,40]
+total= 0
+for price in prices:
+    total=total+price
+    print(f"Total: {total}")
